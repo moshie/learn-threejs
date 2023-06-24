5ffc702e772a7c626472461e732302294c70c303
David Hewitt <hello@wp-unite.io>
2023-06-24 19:07:48 +0100

    lights and shadows

--- src/index.ts
+++ src/index.ts
@@ -51,35 +51,57 @@ camera.position.set(0, 1.5, 3);
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 /**
+ * Materials
+ */
+const flatMaterial = new THREE.MeshStandardMaterial({
+  color: '#fff',
+});
+const shinyMaterial = new THREE.MeshStandardMaterial({
+  color: '#fff',
+  roughness: 0.4,
+});
+
+/**
  * Geometry
  */
-const SphereGeo = new THREE.SphereGeometry(0.5, 20, 20);
-const sphereMaterial = new THREE.MeshStandardMaterial({
-  color: 'green',
-});
-const sphere = new THREE.Mesh(SphereGeo, sphereMaterial);
-sphere.position.y = 0.5;
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 20, 20),
+  shinyMaterial
+);
+sphere.position.y = 1;
 sphere.castShadow = true;
 scene.add(sphere);
 
-const planeGeo = new THREE.PlaneGeometry(4, 4);
-const planeMaterial = new THREE.MeshStandardMaterial({
-  color: '#ccc',
-});
-const plane = new THREE.Mesh(planeGeo, planeMaterial);
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(10, 10), flatMaterial);
 plane.rotateX(-(Math.PI / 2));
 plane.receiveShadow = true;
 scene.add(plane);
 
+const torusKnot = new THREE.Mesh(
+  new THREE.TorusKnotGeometry(0.4, 0.15, 30, 20),
+  shinyMaterial
+);
+torusKnot.position.x = 2;
+torusKnot.position.y = 1;
+torusKnot.castShadow = true;
+scene.add(torusKnot);
+
+const torus = new THREE.Mesh(new THREE.TorusGeometry(0.5, 0.2), shinyMaterial);
+torus.position.x = -2;
+torus.position.y = 1;
+torus.castShadow = true;
+scene.add(torus);
+
 /**
  * Light
  */
-const light = new THREE.DirectionalLight();
-light.castShadow = true;
-light.position.y = 3;
-light.position.x = 3;
+const ambientLight = new THREE.AmbientLight('white', 0.5);
 
-scene.add(light);
+const light = new THREE.PointLight('white', 0.5);
+light.castShadow = true;
+light.position.set(2, 3, 4);
+
+scene.add(ambientLight, light);
 
 /**
  * Resize Handler
@@ -98,6 +120,11 @@ function animate() {
   stats.begin();
 
   requestAnimationFrame(animate);
+
+  torusKnot.rotation.x += 0.01;
+  torusKnot.rotation.y += 0.01;
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.01;
 
   controls.update();
 
